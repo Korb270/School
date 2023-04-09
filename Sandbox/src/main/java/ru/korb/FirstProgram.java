0b1c384db242eec3627306386d834a77729ce223
Korb270 <korb270@yandex.ru>
2023-04-09 13:37:57 +0300

    Вычисление расстояния между двумя точками

--- Sandbox/src/main/java/ru/korb/FirstProgram.java
+++ Sandbox/src/main/java/ru/korb/FirstProgram.java
@@ -2,27 +2,36 @@ package ru.korb;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        hello("world");
-        hello("user");
-        hello("Igor");
+//        hello("world");
+//        hello("user");
+//        hello("Igor");
+//
+//        double l = 7;
+//        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+//
+//        double a = 3;
+//        double b = 6;
+//        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 
-        double l = 7;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-
-        double a = 3;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        Point p1 = new Point(3,5);
+        Point p2 = new Point(3,6);
+        System.out.println("Расстояние между точками равно " + distance(p1, p2));
     }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody + "!");
-    }
+//    public static void hello(String somebody) {
+//        System.out.println("Hello, " + somebody + "!");
+//    }
+//
+//    public static double area(double len) {
+//
+//        return len * len;
+//    }
+//
+//    public static double area(double a, double b) {
+//        return a * b;
+//    }
 
-    public static double area(double len) {
-        return len * len;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
 }
---
+++ Sandbox/src/main/java/ru/korb/Point.java
@@ -0,0 +1,27 @@
+package ru.korb;
+
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+//    public double x1;
+//    public double x2;
+//    public double y1;
+//    public double y2;
+
+//    public Point p1 (double x1,double x2){
+//
+//
+//    }
+//
+//    public Point p2 (double y1, double y2){
+//
+//
+//    }
+}
