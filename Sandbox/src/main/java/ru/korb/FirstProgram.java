0d77f79fd67cd7c7cf2519454b3895e1221965ff
Korb270 <korb270@yandex.ru>
2023-04-10 14:20:30 +0300

    Исправление задания

--- Sandbox/src/main/java/ru/korb/FirstProgram.java
+++ Sandbox/src/main/java/ru/korb/FirstProgram.java
@@ -13,8 +13,8 @@ public class FirstProgram {
 //        double b = 6;
 //        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 
-        Point p1 = new Point(3,5);
-        Point p2 = new Point(3,6);
+        Point p1 = new Point(3, 5);
+        Point p2 = new Point(3, 6);
         System.out.println("Расстояние между точками равно " + distance(p1, p2));
     }
 
