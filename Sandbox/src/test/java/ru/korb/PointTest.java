308af1d92ed2872d020ea5f4eb095fe28418259e
Korb270 <korb270@yandex.ru>
2023-04-12 13:01:22 +0300

    Написание автотеста

---
+++ Sandbox/src/test/java/ru/korb/PointTest.java
@@ -0,0 +1,22 @@
+package ru.korb;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+    @Test
+
+    public void testDistance () {
+        Point p1 = new Point(3, 5);
+        Point p2 = new Point(3, 6);
+        Assert.assertEquals(p1.distance(p2), 1.0);
+
+        Point p3 = new Point(7, 9);
+        Point p4 = new Point(7, 6);
+        Assert.assertEquals(p3.distance(p4), 3.0);
+
+        Assert.assertEquals(p1.distance(p4), 4.123105625617661);
+        Assert.assertEquals(p2.distance(p3), 5);
+        Assert.assertEquals(p2.distance(p2), 0);
+    }
+}
