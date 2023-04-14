92231048364fc6588e21eaa98d8ebdf50c530720
Korb270 <korb270@yandex.ru>
2023-04-14 10:21:54 +0300

    Исправление задания 3

--- Sandbox/src/test/java/ru/korb/PointTest.java
+++ Sandbox/src/test/java/ru/korb/PointTest.java
@@ -4,19 +4,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTest {
+    Point p1 = new Point(3,5);
+    Point p2 = new Point(3, 6);
+    Point p3 = new Point(3.6, 9.7);
+    Point p4 = new Point(7.2, 4.6);
+    Point p5 = new Point(-4, 8);
     @Test
-
+    // Проверяем с целыми числами
     public void testDistance () {
-        Point p1 = new Point(3, 5);
-        Point p2 = new Point(3, 6);
         Assert.assertEquals(p1.distance(p2), 1.0);
-
-        Point p3 = new Point(7, 9);
-        Point p4 = new Point(7, 6);
-        Assert.assertEquals(p3.distance(p4), 3.0);
-
-        Assert.assertEquals(p1.distance(p4), 4.123105625617661);
-        Assert.assertEquals(p2.distance(p3), 5);
+    }
+    @Test
+    // Проверяем с дродными числами
+    public void testDistanceFraction () {
+        Assert.assertEquals(p3.distance(p4), 6.2425956140054435);
+    }
+    @Test
+    //Проверяем если координаты точек совпали
+    public void testDistanceIdentical () {
         Assert.assertEquals(p2.distance(p2), 0);
+    }
+    @Test
+    //Проверяем отрицательные числа
+    public void testDistanceNegNum (){
+        Assert.assertEquals(p5.distance(p2), 7.280109889280518);
     }
 }
